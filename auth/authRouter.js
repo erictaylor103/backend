c667c714039ceccbe475a82cf20428cfd5fe38d6
Eric Taylor <erictaylor@Erics-MacBook-Pro.local>
2020-06-22 17:14:09 -0400

    add validateUser file

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-//const validateUser = require('../middleware/validateuser.js');
+const validateUser = require('../middleware/validateuser.js');
 const { register, login } = require('./authController');
 
 
-router.post('/register', register);
+router.post('/register', validateUser, register);
 router.post('/login', login);
 
 
